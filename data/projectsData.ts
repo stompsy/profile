fa699f967ccbd11b3a881ba1bf38101348b8e34a
stompsy <brian.gerdes@protonmail.com>
2024-01-01 21:18:53 -0800

    add project, cleanup

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,7 +9,13 @@ const projectsData = [
     title: 'Practice Responsive Portfolio',
     description: `An assignment for freeCodeCamp written in HTML & CSS. The goal was to make a responsive portfolio page.`,
     imgSrc: '/static/images/freeCodeCamp_responsive_portfolio.png',
-    href: 'https://styrbjorne.github.io/practice-responsive-portfolio/',
+    href: 'https://prp.stompsy.dev/',
+  },
+  {
+    title: 'Django Portfolio',
+    description: `A basic portfolio with Django written in Python. My goal was to understand how to use Django to create a responsive portfolio presentation page.`,
+    imgSrc: '/static/images/django-portfolio.png',
+    href: 'https://github.com/stompsy/django-portfolio',
   },
 ]
 
