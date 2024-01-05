d6d66347419483486a4b6c32d276b96b0d974f82
Brian Gerdes <brian.gerdes@protonmail.com>
2024-01-05 02:08:09 -0800

    Chore: Add two projects to profile and images associated with them

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -17,6 +17,18 @@ const projectsData = [
     imgSrc: '/static/images/django-portfolio.png',
     href: 'https://github.com/stompsy/django-portfolio',
   },
+  {
+    title: 'Mock Nike Landing Page',
+    description: `A mock landing page for Nike. The goal was to practice using Tailwindcss.`,
+    imgSrc: '/static/images/mock-nike-landing-page.png',
+    href: 'https://nike-tailwind.stompsy.dev/',
+  },
+  {
+    title: 'Markdown Previewer',
+    description: `A basic markdown previewer. It was an assignment for freeCodeCamp. Built using React, Tailwindcss, and Remarkable.`,
+    imgSrc: '/static/images/markdown-previewer.png',
+    href: 'https://mdview.stompsy.dev/',
+  },
 ]
 
 export default projectsData
